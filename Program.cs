95ecbbde9ebd31d099bac078a46a0c6dd3399316
Jocimar B. A. Huss <jocimarspi@gmail.com>
2019-08-02 20:44:56 -0300

    Fist commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MentoriaCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
